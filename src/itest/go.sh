84618a2dd0255925e5afe1235af0e38910cc6902
Alex Davydov <alex-d@live.co.uk>
2025-02-14 13:16:26 +0100

    filter apply

--- src/itest/go.sh
+++ src/itest/go.sh
@@ -4,5 +4,6 @@ python ../main.py -w params_1.json input.tsv output_1.tsv
 python ../main.py -w params_2.json input.tsv output_2.tsv
 python ../main.py -w params_3.json input.tsv output_3.tsv
 python ../main.py -w params_4.json input.tsv output_4.tsv
+python ../main.py -w params_5.json input.tsv output_5.tsv
 python ../main.py -w params_1.json input_empty.tsv output_empty_1.tsv
 python ../main.py -w params_2.json input_empty.tsv output_empty_2.tsv
