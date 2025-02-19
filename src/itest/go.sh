a8f5819e330fc5474a498faad764a9a762d140c8
Alex Davydov <alex-d@live.co.uk>
2025-02-19 12:38:18 +0100

    itest for transform

--- src/itest/go.sh
+++ src/itest/go.sh
@@ -5,5 +5,8 @@ python ../main.py -w params_2.json input.tsv output_2.tsv
 python ../main.py -w params_3.json input.tsv output_3.tsv
 python ../main.py -w params_4.json input.tsv output_4.tsv
 python ../main.py -w params_5.json input.tsv output_5.tsv
+echo "test"
+python ../main.py -w params_6.json input.tsv output_6.tsv
+echo "test"
 python ../main.py -w params_1.json input_empty.tsv output_empty_1.tsv
 python ../main.py -w params_2.json input_empty.tsv output_empty_2.tsv
